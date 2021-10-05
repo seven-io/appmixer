fffea8b9cf6ce24f6d5be14e734537c2545fb3e3
André Matthies <12965261+matthiez@users.noreply.github.com>
2021-10-05 15:54:36 +0200

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+![](https://seven.io/seven_outline_white.svg "seven Logo")
+
+A collection of Appmixer actions for seven.
+
+## Installation
+
+```shell
+git clone https://github.com/seven-io/appmixer
+```
+
+```shell
+cd appmixer/appmixer.seven/appmixer/seven
+```
+
+```shell
+appmixer publish
+```
+
+### Set API Key
+
+Add an environment variable called `SEVEN_API_KEY`.
+
+## Support
+
+Need help? Feel free to [contact us](https://www.sms77.io/en/company/contact).
+
+[![MIT](https://img.shields.io/badge/License-MIT-teal.svg)](LICENSE)
---
+++ appmixer.seven/appmixer/seven/sms/SevenSms/SevenSms.js
@@ -0,0 +1,24 @@
+const Sms77Client = require('sms77-client')
+
+if (!globalThis.fetch) globalThis.fetch = require('node-fetch')
+
+module.exports = {
+    async receive(context) {
+        const {from, text, to} = context.messages.in.content
+        const client = new Sms77Client(process.env.SEVEN_API_KEY, 'AppMixer')
+        const params = {
+            from,
+            json: true,
+            text,
+            to,
+        }
+
+        console.log('params', params)
+
+        const response = await client.sms(params)
+
+        console.log('response', response)
+
+        return context.sendJson({response}, 'out')
+    }
+}
